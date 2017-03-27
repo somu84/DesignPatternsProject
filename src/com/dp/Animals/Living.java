cc1cf4223267a40422f87118f45d1c497d41866a
Someshwar Chatterjee <someshwarchatterjee@gmail.com>
2017-03-26 23:02:52 -0400

    Interfaces demo

---
+++ src/com/dp/Animals/Living.java
@@ -0,0 +1,22 @@
+package com.dp.Animals;
+
+public interface Living {
+
+	public void setName(String newName);
+	public String getName();
+	
+	public void setHeight(double newHeight);
+	public double getHeight();
+	
+	public void setWeight(int newWeight);
+	public int getWeight();
+	
+	public void setFavFood(String newFood);
+	public String getFavFood();
+	
+	public void setSpeed(double newSpeed);
+	public double getSpeed();
+	
+	public void setSound(String newSound);
+	public String getSound();
+}
