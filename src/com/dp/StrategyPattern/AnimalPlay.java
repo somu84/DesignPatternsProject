fb688dbdf08a829cf8419d81f068cc7a080859fc
somu84 <someshwarchatterjee@gmail.com>
2017-04-01 18:09:44 -0400

    more description about the dp.

--- src/com/dp/StrategyPattern/AnimalPlay.java
+++ src/com/dp/StrategyPattern/AnimalPlay.java
@@ -2,6 +2,10 @@ package com.dp.StrategyPattern;
 
 /**
  * The Strategy DP, lets the algorithm vary independently from the client that use it.
+ * 
+ * Here we had two algorithms (ItFlys and CantFly). We encapsulated them, under an interface(Flys)
+ * and made them interchangeable.
+ * 
  * @author Somu
  *
  */
